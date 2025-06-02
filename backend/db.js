3f0b763ae1272a9b2cc988b6f56f4c794123e6ad
Shrxsth Bansal <shrxsth16@gmail.com>
2025-06-02 08:49:54 +0530

    adding db

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017";
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURI);
+        console.log("Connected to Mongo Successfully");
+    } catch (err) {
+        console.error("Failed to connect to MongoDB:", err);
+    }
+};
+
+module.exports = connectToMongo;
