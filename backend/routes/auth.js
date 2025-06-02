600ba64b9178a2372bc8bfe0faac8397201215a2
Shrxsth Bansal <shrxsth16@gmail.com>
2025-06-02 09:43:57 +0530

    added apis

---
+++ backend/routes/auth.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const User=require('../models/User');
+const router=express.Router();
+
+//create a user using : post "api/auth/". doesn;t require auth
+router.post('/',(req,res)=>{
+    console.log(req.body);
+    const user=User(req.body);
+    user.save()
+    res.send(req.body);
+})
+module.exports=router
